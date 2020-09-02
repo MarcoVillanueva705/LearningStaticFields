a75af82dfc177fcc7605b13746647ed715739774
marco villanueva <villanuevamarco98@gmail.com>
2020-09-01 20:25:20 -0600

    notes on namespace and class usage

--- LearningStaticFields/Util.cs
+++ LearningStaticFields/Util.cs
@@ -8,7 +8,10 @@ namespace Util
     {
         static public string Ask(string question)
         {
-        
+            //class function 'Ask' poses questin and returns
+            //user input on next line
+            //see line 28 of Program.cs
+            //for refactored code that replaces Write and Read Lines
             Console.Write(question);
             return Console.ReadLine();
         }
