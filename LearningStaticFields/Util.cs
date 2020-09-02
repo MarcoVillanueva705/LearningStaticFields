a8133e2e94deb0ef93593a1673c4e8946f8673d0
marco villanueva <villanuevamarco98@gmail.com>
2020-09-01 20:13:31 -0600

    separate utils class for use with program

---
+++ LearningStaticFields/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util
+{
+    class AskAQuestion
+    {
+        static public string Ask(string question)
+        {
+        
+            Console.Write(question);
+            return Console.ReadLine();
+        }
+    }
+}
